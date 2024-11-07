bf8a9a36765523fc2e3092196da4ee8557248a97
PrajwalMadikai <133801108+PrajwalMadikai@users.noreply.github.com>
2024-11-07 16:41:07 +0530

    final commit

--- index.js
+++ index.js
@@ -23,10 +23,12 @@ function getWeather(event) {
 }
 
 function displayWeather(data) {
-    if (data.cod === '404') {
-        alert('City not found. Please try again.');
+if (data.cod === '404') {
+    document.querySelector('.error').innerHTML="Error finding City.Please try again"
+    document.querySelector('.error').style.display = 'block';
+
     } else if(data.main && data.weather){
-        const temperature = data.main.temp;
+        const temperature = data.main.temp.toFixed(2);
         const cityName = data.name;
         const humidity = data.main.humidity;
         const windSpeed = data.wind.speed;
@@ -35,5 +37,29 @@ function displayWeather(data) {
         document.getElementById('degree').innerHTML = `${temperature}°C`;
         document.getElementById('hum').innerHTML = `${humidity} %`;
         document.getElementById('speed').innerHTML = `${windSpeed} km/h`;
+
+        const weatherIcon = document.querySelector('.weather-icon');
+        
+        if (temperature >=10 && temperature <= 20) {
+            weatherIcon.src = './images/clouds.png';
+        } else if (temperature >20 && temperature <=30) {
+            weatherIcon.src = './images/sun.png';
+        } else if (temperature >30 && temperature <=40) {
+            weatherIcon.src = './images/hot-temperature.png';
+        } else if (temperature <10) {
+            weatherIcon.src = './images/snow.png';
+        } else {
+            // weatherIcon.src =  ; // Default icon if no match
+        }
     }
+}
+function showError(message) {
+    const errorDiv = document.querySelector('.error');
+    errorDiv.innerHTML = "Error finding City.Please try again";
+    errorDiv.style.display = 'block';
+
+    // Automatically hide the error after 3 seconds
+    setTimeout(() => {
+        errorDiv.style.display = 'none';
+    }, 2000);
 }
